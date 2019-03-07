a59c3486cd75d73f0fce5f806a3d222180ff8e5b
Your Name <you@example.com>
2019-03-07 15:39:41 +0900

    add xcode project

---
+++ BitriseStartup/ViewController.swift
@@ -0,0 +1,19 @@
+// 
+//  BitriseStartup
+//
+//  Created by Naoki Fujii.
+//  Copyright (c) 2018 naokinkfj All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
